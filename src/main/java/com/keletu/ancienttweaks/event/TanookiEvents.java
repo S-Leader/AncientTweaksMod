276eb4eef34361b44eda3501c6607990e8fc42ed
a <EYCHFSJD@gmail.com>
2026-05-31 12:38:39 +0800

    EMERGENCY FIX.

--- src/main/java/com/keletu/ancienttweaks/event/TanookiEvents.java
+++ src/main/java/com/keletu/ancienttweaks/event/TanookiEvents.java
@@ -86,7 +86,7 @@ public class TanookiEvents {
 
         var jumpHeight = player.getAttribute(Attributes.JUMP_STRENGTH);
         if (jumpHeight != null && jumpHeight.getModifier(STATUE_JUMP_ID) == null) {
-            jumpHeight.addPermanentModifier(new AttributeModifier(STATUE_JUMP_ID, 0, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
+            jumpHeight.addPermanentModifier(new AttributeModifier(STATUE_JUMP_ID, -10, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
         }
     }
 
